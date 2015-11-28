bc267b99bc20c1d9302f5917bb577ba118346059
kercobo <mib.iqbaal@gmail.com>
2015-11-28 14:08:19 +0800

    add chart into the dashboard

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -12,7 +12,7 @@ class StatsController extends BaseController
 
     public function getIndex()
     {
-        return View('stats');
+        return View('indexs');
     }
 
     public function getAPI()
@@ -21,16 +21,51 @@ class StatsController extends BaseController
 
         $range = \Carbon\Carbon::now()->subDays($days);
 
+        if(\Auth::user()->username == 'sengkol'){
+
         $stats = DB::table('analytics.kartu_anc_registration')
             ->join('analytics.kartu_ibu_registration', 'analytics.kartu_ibu_registration.kiid', '=', 'analytics.kartu_anc_registration.kiid')
              ->whereBetween('analytics.kartu_anc_registration.submissiondate', ['2015-11-01', '2015-11-31'])
+           //  ->where('analytics.kartu_anc_registration.userId', )
 //            ->where('analytics.kartu_anc_registration.submissiondate', '>=', $range)
             ->groupBy('analytics.kartu_ibu_registration.desa')
             ->get([
-                DB::raw('analytics.kartu_ibu_registration.desa as date'),
+                DB::raw('analytics.kartu_ibu_registration.desa as dusun'),
                 DB::raw('COUNT(*) as jumlah')
             ]);
 
         return $stats;
+        }
+        
+        if(\Auth::user()->username == 'janapria'){
+
+        $stats = DB::table('analytics.kartu_anc_registration')
+            ->join('analytics.kartu_ibu_registration', 'analytics.kartu_ibu_registration.kiid', '=', 'analytics.kartu_anc_registration.kiid')
+             ->whereBetween('analytics.kartu_anc_registration.submissiondate', ['2015-11-01', '2015-11-31'])
+           //  ->where('analytics.kartu_anc_registration.puskesmas', 'janapria')
+//            ->where('analytics.kartu_anc_registration.submissiondate', '>=', $range)
+            ->groupBy('analytics.kartu_ibu_registration.desa')
+            ->get([
+                DB::raw('analytics.kartu_ibu_registration.desa as dusun'),
+                DB::raw('COUNT(*) as jumlah')
+            ]);
+
+        return $stats;
+        }
+
+        else{
+
+        $stats = DB::table('analytics.kartu_anc_registration')
+            ->join('analytics.kartu_ibu_registration', 'analytics.kartu_ibu_registration.kiid', '=', 'analytics.kartu_anc_registration.kiid')
+             ->whereBetween('analytics.kartu_anc_registration.submissiondate', ['2015-11-01', '2015-11-31'])
+             ->where('analytics.kartu_anc_registration.userId', \Auth::user()->username)
+            ->groupBy('analytics.kartu_ibu_registration.dusun')
+            ->get([
+                DB::raw('analytics.kartu_ibu_registration.dusun as dusun'),
+                DB::raw('COUNT(*) as jumlah')
+            ]);
+
+        return $stats;
+        }
     }
 }
--- resources/views/indexs.blade.php
+++ resources/views/indexs.blade.php
@@ -16,8 +16,79 @@
 			
 	
 	</div>
-	
+<script src="//cdn.jsdelivr.net/spinjs/1.3.0/spin.min.js"></script>
+<div class="row">
+    <div class="col-md-12">
+        <h1>Cakupan KI Bulan ini</h1>
+
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <div id="stats-container" align="center" style="height: 250px;">Cakupan K1 Bulan ini</div>
+    </div>
+</div>
+<script>
+    $(function () {
+
+        // Define the area for the spinner
+        var spinTarget = document.getElementById('stats-container');
+
+        // Create a function that will handle AJAX requests
+        function requestData(days, chart) {
+            // Activate the spinner
+            var spinner = new Spinner().spin(spinTarget);
+
+            $.ajax({
+                type: "GET",
+                dataType: 'json',
+                url: "./api", // This is the URL to the API
+                data: { days: days }
+            })
+                .done(function (data) {
+                    // When the response to the AJAX request comes back render the chart with new data
+                    chart.setData(data);
+                })
+                .fail(function () {
+                    // If there is no communication between the server, show an error
+                    alert("error occured");
+                })
+                .always(function () {
+                    // No matter if request is successful or not, stop the spinner
+                    spinner.stop();
+                });
+        }
+
+        var chart = Morris.Bar({
+            // ID of the element in which to draw the chart.
+            element: 'stats-container',
+            data: [0, 0], // Set initial data (ideally you would provide an array of default data)
+            xkey: 'dusun', // Set the key for X-axis
+            ykeys: ['jumlah'], // Set the key for Y-axis
+            labels: ['jumlah'] // Set the label when bar is rolled over
+        });
+
+        // Request initial data 
+        requestData(31, chart);
+
+        $('ul.ranges a').click(function (e) {
+            e.preventDefault();
+
+            // Get the number of days from the data attribute
+            var el = $(this);
+            days = el.attr('data-range');
+
+            // Request the data and render the chart using our handy function
+            requestData(days, chart);
+
+            // Make things pretty to show which button/tab the user clicked
+            el.parent().addClass('active');
+            el.parent().siblings().removeClass('active');
+        })
+    });
+</script>
 
 	
 </div>
+
 @stop
